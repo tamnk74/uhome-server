3388cee1c84d3a6e3e21c3904b5c1f58019fb170
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-04-06 21:41:29 +0700

    feature: Add attachments to issue detail

--- server/modules/issues/services/issue.js
+++ server/modules/issues/services/issue.js
@@ -1,8 +1,11 @@
 import Issue from '../../../models/issue';
+import Attachment from '../../../models/attachment';
+import Category from '../../../models/category';
 
 export default class IssueService {
   static async create(issue) {
-    return Issue.addIssue(issue);
+    issue = await Issue.addIssue(issue);
+    return this.getDetail(issue.id);
   }
 
   static async remove(issue) {
@@ -10,6 +13,29 @@ export default class IssueService {
   }
 
   static async getDetail(id) {
-    return Issue.findByPk(id);
+    return Issue.findByPk(id, {
+      include: [
+        {
+          model: Category,
+          required: false,
+          as: 'categories',
+        },
+        {
+          model: Attachment,
+          as: 'attachments',
+          require: false,
+          attributes: [
+            'id',
+            'size',
+            'mimeType',
+            'createdAt',
+            'updatedAt',
+            'issueId',
+            'path',
+            Attachment.buildUrlAttribuiteSelect(),
+          ],
+        },
+      ],
+    });
   }
 }
