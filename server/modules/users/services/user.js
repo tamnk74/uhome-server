cbf66df96303ad1ea46324ad6a137e29c6abe19d
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-04-09 09:42:36 +0700

    feature: Set pre commit github

--- server/modules/users/services/user.js
+++ server/modules/users/services/user.js
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from 'uuid';
-import { Op } from 'sequelize';
 import { fileSystemConfig } from '../../../config';
 import Issue from '../../../models/issue';
 import Category from '../../../models/category';
