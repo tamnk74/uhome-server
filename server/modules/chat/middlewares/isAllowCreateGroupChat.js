d2ede039fca46851bbbd86d397ad9fd74b7dbc69
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-05-29 16:21:49 +0700

    UH-93: Update validate create chat group

--- server/modules/chat/middlewares/isAllowCreateGroupChat.js
+++ server/modules/chat/middlewares/isAllowCreateGroupChat.js
@@ -4,13 +4,14 @@ import errorFactory from '../../../errors/ErrorFactory';
 export const isAllowCreateGroupChat = async (req, res, next) => {
   try {
     const { issueId, userId } = req.body;
+    const { user } = req;
     const issue = await Issue.findByPk(issueId);
 
     if (!issue) {
       throw errorFactory.getError('ISSU-0001');
     }
 
-    if (issue.createdBy === userId) {
+    if (user.id === userId) {
       throw errorFactory.getError('CHAT-0203');
     }
 
