dc877a0b4e729795012ab5124f429d67c72a65fb
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-07-31 09:31:05 +0700

    [UH-243] Implement basic authentication

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,5 +1,6 @@
 import auth from './auth';
 import validator from './validator';
 import active from './active';
+import basicAuth from './basic_auth';
 
-export { auth, validator, active };
+export { auth, validator, active, basicAuth };
