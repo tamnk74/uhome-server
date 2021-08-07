e4e3fec180c02d075ef94da15d06b5ef628aa538
khactam94 <khac.tam.94@gmail.com>
2021-08-07 21:03:54 +0700

    feat: pay in account and payment issue

--- server/modules/payment/controllers/payment.js
+++ server/modules/payment/controllers/payment.js
@@ -2,10 +2,22 @@ import { objectToSnake } from '../../../helpers/Util';
 import { PaymentService } from '../services';
 
 export default class PaymentController {
-  static async process(req, res, next) {
+  static async payin(req, res, next) {
     try {
-      const { receiveIssue, user } = req;
-      const result = await PaymentService.process(receiveIssue, req.body, { user });
+      const { user } = req;
+      const result = await PaymentService.process(user, req.body);
+
+      return res.status(201).json(objectToSnake(result));
+    } catch (e) {
+      return next(e);
+    }
+  }
+
+  static async payment(req, res, next) {
+    try {
+      const { user, issue } = req;
+      const result = await PaymentService.payment({ user, issue });
+
       return res.status(201).json(objectToSnake(result));
     } catch (e) {
       return next(e);
