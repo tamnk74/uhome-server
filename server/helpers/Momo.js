2b20d46b095c57db8315f335e007d3ba0d6f1a9e
khactam94 <khac.tam.94@gmail.com>
2021-08-24 20:55:45 +0700

    feat: momo payment

--- server/helpers/Momo.js
+++ server/helpers/Momo.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import { momoConfig } from '../config';
 
+global.navigator = { appName: 'nodejs' }; // fake the navigator object
+global.window = {}; // fake the window object
+const JSEncrypt = require('jsencrypt');
 const NodeRSA = require('node-rsa');
 const crypto = require('crypto');
 
@@ -10,10 +13,24 @@ export class Momo {
       encoding: 'utf8',
       flag: 'r',
     });
+
     const key = new NodeRSA(publicKey, { encryptionScheme: 'pkcs1' });
     const encryptData = key.encrypt(JSON.stringify(data), 'base64');
 
     return encryptData.toString('base64');
+  }
+
+  static async jsEncryptRSA(data) {
+    const publicKey = await fs.readFileSync(momoConfig.publicKeyFile, {
+      encoding: 'utf8',
+      flag: 'r',
+    });
+    console.log(publicKey);
+    const encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKey);
+    const encrypted = encrypt.encrypt(JSON.stringify(data));
+
+    return encrypted;
   }
 
   static createSignature(data) {
