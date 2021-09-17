3066302cd9469418dde13facf387b42ff1baa5e9
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-09-17 22:49:15 +0700

    Update event api

--- server/modules/event/routes/index.js
+++ server/modules/event/routes/index.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import EventController from '../controllers/event';
 import { auth } from '../../../middlewares';
 
 const router = Router();
 
+const storage = multer.memoryStorage({
+  destination(req, file, callback) {
+    callback(null, '');
+  },
+});
+const upload = multer({ storage }).single('file');
+
 router.get('/', auth, EventController.index);
+router.patch('/:id', auth, upload, EventController.update);
 
 export default router;
