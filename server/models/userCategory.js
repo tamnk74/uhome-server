96cda865766e416c66168cb4eb822bc66ed4fb83
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-09-28 13:11:24 +0700

    Add category to consulting account

---
+++ server/models/userCategory.js
@@ -0,0 +1,43 @@
+import Sequelize from 'sequelize';
+import uuid from 'uuid';
+import BaseModel from './model';
+import sequelize from '../databases/database';
+
+class UserCategory extends BaseModel {}
+
+UserCategory.init(
+  {
+    userId: {
+      type: Sequelize.DataTypes.UUID,
+      allowNull: false,
+    },
+    categoryId: {
+      type: Sequelize.DataTypes.UUID,
+      allowNull: false,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defautValue: Sequelize.NOW,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defautValue: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: 'user_category',
+    tableName: 'user_category',
+    freezeTableName: true,
+  }
+);
+UserCategory.removeAttribute('id');
+UserCategory.removeAttribute('deletedAt');
+
+UserCategory.beforeCreate((instance) => {
+  instance.id = uuid.v4();
+});
+
+module.exports = UserCategory;
