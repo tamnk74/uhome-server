702cd7843268f8fcc0f42c7a7af075f287d62801
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2021-11-18 23:52:02 +0700

    Update the material attribute chat message

--- server/modules/chat/schema/approveMaterialCostSchema.js
+++ server/modules/chat/schema/approveMaterialCostSchema.js
@@ -1,7 +1,11 @@
 import Joi from 'joi';
 
+const materialItem = Joi.object().keys({
+  cost: Joi.number().integer().min(0).required(),
+  material: Joi.string().required(),
+});
+
 export const approveMaterialCostSchema = Joi.object().keys({
-  total_cost: Joi.number().integer().min(0).required(),
-  materials: Joi.string().required(),
+  materials: Joi.array().items(materialItem).required(),
   message_sid: Joi.string().required(),
 });
