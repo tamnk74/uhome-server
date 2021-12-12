d0728462e59b15881ef62622777f0421a8b8521f
Tam Nguyen Khac <khac.tam.94@gmail.com>
2021-12-12 11:57:43 +0700

    feat: check verified user

--- server/middlewares/verified.js
+++ server/middlewares/verified.js
@@ -1,13 +1,17 @@
 import errorFactory from '../errors/ErrorFactory';
+import User from '../models/user';
 import { status } from '../constants';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const { user } = req;
   if (user.status === status.IN_ACTIVE) {
     return next(errorFactory.getError('USER-0001'));
   }
-  if (!user.verifiedAt) {
-    return next(errorFactory.getError('USER-0003'));
+  if (!user.verified_at) {
+    const updatedUser = await User.findByPk(user.id);
+    if (!updatedUser.verifiedAt) {
+      return next(errorFactory.getError('USER-0003'));
+    }
   }
   return next();
 };
