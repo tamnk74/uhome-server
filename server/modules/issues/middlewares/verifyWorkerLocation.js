5de3a6c3d3107f0e020dbef35fb6800f3be4fa6e
Tam Nguyen Khac <khac.tam.94@gmail.com>
2021-12-27 21:59:09 +0700

    feat: verify worker location

---
+++ server/modules/issues/middlewares/verifyWorkerLocation.js
@@ -0,0 +1,26 @@
+import { googleMap } from '../../../helpers';
+import errorFactory from '../../../errors/ErrorFactory';
+import User from '../../../models/user';
+
+export const verifyWorkerLocation = async (req, res, next) => {
+  const { user } = req;
+  const updatedUser = await User.findByPk(user.id);
+  if (!updatedUser) {
+    return next(errorFactory.getError('USER-0002'));
+  }
+
+  const { lat, lon } = updatedUser;
+  if (!lat || !lon) {
+    return next(errorFactory.getError('ISSUE-0416'));
+  }
+
+  const allowedProvinces = ['Đà Nẵng', 'Quang Nam Province', 'Quảng Nam', 'Da Nang'];
+  const province = await googleMap.getProvince({
+    lat,
+    lng: lon,
+  });
+  if (!allowedProvinces.includes(province)) {
+    return next(errorFactory.getError('ISSUE-0415'));
+  }
+  return next();
+};
