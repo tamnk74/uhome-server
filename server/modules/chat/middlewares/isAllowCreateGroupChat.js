9f8bd0901d3bec5ba63def727747ab3f0a498ab7
Duy Ho Ngoc <duyhn@tech.est-rouge.com>
2022-01-14 23:12:05 +0700

    UH-616: Prevent user access to chat history of worker

--- server/modules/chat/middlewares/isAllowCreateGroupChat.js
+++ server/modules/chat/middlewares/isAllowCreateGroupChat.js
@@ -1,17 +1,28 @@
-import Issue from '../../../models/issue';
+import RequestSupporting from '../../../models/requestSupporting';
 import errorFactory from '../../../errors/ErrorFactory';
+import { roles } from '../../../constants';
 
 export const isAllowCreateGroupChat = async (req, res, next) => {
   try {
-    const { issueId, userId } = req.body;
-    const { user } = req;
-    const issue = await Issue.findByPk(issueId);
-
-    if (!issue) {
-      return next(errorFactory.getError('ISSU-0001'));
-    }
+    const { userId } = req.body;
+    const { user, issue } = req;
 
     if (user.id === userId) {
+      return next(errorFactory.getError('CHAT-0203'));
+    }
+
+    if (user.role !== roles.USER) {
+      return next();
+    }
+
+    const requestSupporting = await RequestSupporting.findOne({
+      where: {
+        issueId: issue.id,
+        userId: [user.id, userId],
+      },
+    });
+
+    if (!requestSupporting) {
       return next(errorFactory.getError('CHAT-0203'));
     }
 
